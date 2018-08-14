830ea9122f68d29c095c8bac781fa46ccd70777a
IamWangYunKai <615349118@163.com>
2018-08-14 16:50:29 +0800

    cmake

---
+++ cmake_projects/helloworld/src/hello.cpp
@@ -0,0 +1,5 @@
+﻿#include "hello.h"
+
+void hello(){
+	std::cout << "Hello World !" << std::endl;
+}
---
+++ cmake_projects/helloworld/src/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "hello.h"
+
+using namespace std;
+int main() {
+	hello();
+	system("pause");
+	return 0;
+}
