177a4099688dfd77360cff2fb5ae4409665c3854
IamWangYunKai <615349118@163.com>
2018-08-16 15:22:04 +0800

    test camera

--- cmake_projects/merge/src/main.cpp
+++ cmake_projects/merge/src/main.cpp
@@ -4,53 +4,27 @@
 #include <cmath>
 #include <time.h>
 #include <vector>
-#include <fstream>
-#include <sstream>
-
 #include <pcl/io/pcd_io.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/features/eigen.h>
-#include <pcl/visualization/cloud_viewer.h>
-#include <pcl/segmentation/region_growing.h>
-#include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/common/transforms.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/features/eigen.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/segmentation/region_growing.h>
+#include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/passthrough.h>
-
-#include <pmdsdk2.h>
-
-#define SOURCE_PLUGIN "O3D3xxCamera.W64.pap"
-#define SOURCE_PARAM "192.168.0.70:80:50010" // Ip Address : port Number : XmlRPC port
-#define PROC_PLUGIN "O3D3xxProc.W64.ppp"
-#define PROC_PARAM ""
-
-#define EXPOSURE 4000
-#define WRITE_DATA false
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 
 #define DEBUG false
 using namespace std;
 
-PMDHandle hnd; // connection handle
-int res;
-PMDDataDescription dd;
-unsigned int integrationTime;
-vector<float> amp;
-vector<float> dist;
-vector<unsigned> flags;
-vector<float> xyz3Dcoordinate;
-int imgHeight;
-int imgWidth;
-char err[256];
 pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-int read_data(void);
-
 int main() {
-	read_data();
+	//read_data();
 	cout << "Reading Using Time : " << (double)clock() / CLOCKS_PER_SEC << "s" << endl;
-	//根据摄像头内参坐标转换
+	//根据摄像头外参坐标转换
 	Eigen::Affine3f transform = Eigen::Affine3f::Identity();
 	float theta_x = - 20 * M_PI / 180.0f;
 	transform.rotate(Eigen::AngleAxisf(theta_x, Eigen::Vector3f::UnitX()));
@@ -250,126 +224,4 @@ int main() {
 	}
 	
 	return 0;
-}
-
-
-int read_data(void){
-	printf("\n Connecting to camera : \n");
-	// connect to camera
-	res = pmdOpen(&hnd, SOURCE_PLUGIN, SOURCE_PARAM, PROC_PLUGIN, PROC_PARAM);
-	if (res != PMD_OK){
-		fprintf(stderr, "Could not connect: \n");
-		getchar();
-		return -1;
-	}
-	printf("\n DONE");
-	printf("\n -----------------------------------------------------------------------------");
-	printf("\n Updating the framedata ");
-	res = pmdUpdate(hnd); // to update the camera parameter and framedata
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 256);
-		fprintf(stderr, "Could not updateData: %s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return -2;
-	}
-	printf("\n DONE");
-	printf("\n -----------------------------------------------------------------------------");
-	printf("\n Retrieving source data description\n");
-	res = pmdGetSourceDataDescription(hnd, &dd);
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not get data description: %s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return -3;
-	}
-	printf("\n DONE");
-
-	imgWidth = dd.img.numColumns;
-	imgHeight = dd.img.numRows;
-	printf("\n -----------------------------------------------------------------------------");
-	dist.resize(imgWidth * imgHeight);
-	amp.resize(imgWidth * imgHeight);
-	flags.resize(imgWidth * imgHeight);
-	xyz3Dcoordinate.resize(imgHeight * imgWidth * 3); // value three is for 3 images
-	printf("\n Obtaining different image data from camera viz amplitude and Distance Image  \n");
-
-	res = pmdGetDistances(hnd, &dist[0], dist.size() * sizeof(float));
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not get distance data: %s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return -4;
-	}
-	printf("\n Middle pixel Distance value: %fm\n", dist[(imgHeight / 2) * imgWidth + imgWidth / 2]);
-
-	res = pmdGetAmplitudes(hnd, &amp[0], amp.size() * sizeof(float));
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not get amplitude data: %s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return -5;
-	}
-	printf("\n Middle Amplitude: %f\n", amp[(dd.img.numRows / 2) * dd.img.numColumns + dd.img.numColumns / 2]);
-
-	res = pmdGetFlags(hnd, &flags[0], flags.size() * sizeof(float));
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not get flag data: %s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return -6;
-	}
-
-	res = pmdGet3DCoordinates(hnd, &xyz3Dcoordinate[0], xyz3Dcoordinate.size() * sizeof(float));
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not get xyz data: %s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return -7;
-	}
-
-	for (int i = 0; i < xyz3Dcoordinate.size(); i++) {
-		if (!(flags[i] & 1)) { // first bit is set to 1,if pixel is invalid
-			input_cloud->push_back(pcl::PointXYZ(xyz3Dcoordinate[i * 3 + 0], 
-												xyz3Dcoordinate[i * 3 + 1], 
-												xyz3Dcoordinate[i * 3 + 2]));
-		}
-	}
-	printf("\n -----------------------------------------------------------------------------");
-	printf("\n Setting and getting parameters viz : integrationTime \n");
-	res = pmdSetIntegrationTime(hnd, 0, EXPOSURE);
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not set the integration time: %s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return 0;
-	}
-
-	res = pmdGetIntegrationTime(hnd, &integrationTime, 0);
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not set the integration time:%s\n", err);
-		pmdClose(hnd);
-		getchar();
-		return 0;
-	}
-	printf("\n integrationTime: %d\n", integrationTime);
-	printf("\n DONE");
-	printf("\n -----------------------------------------------------------------------------");
-	printf("\n Closing the connection \n");
-	res = pmdClose(hnd);
-	if (res != PMD_OK){
-		pmdGetLastError(hnd, err, 128);
-		fprintf(stderr, "Could not close the connection %s\n", err);
-		return 0;
-	}
-	printf("\n Camera closed Successfully");
-	printf("\n ================================================================================");
-	getchar();
 }
---
+++ cmake_projects/test_camera/include/camera.h
@@ -0,0 +1,30 @@
+﻿#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <iostream>
+#include <vector>
+#include <pmdsdk2.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+
+class Camera {
+public:
+	Camera();
+	~Camera();
+	bool setExposureTime(unsigned int exposure);
+	unsigned int getExposureTime();
+	bool getData(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud);
+	bool writeData();
+private:
+	PMDHandle hnd;
+	int res;
+	PMDDataDescription dd;
+	unsigned int exposureTime;
+	//std::vector<float> amp;
+	//std::vector<unsigned> flags;
+	int imgHeight;
+	int imgWidth;
+	char err[256];
+}
+
+#endif
